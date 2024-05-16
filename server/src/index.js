baa2b15b87a15d365a3fc35534b29ecf3d2570dd
Jorge Sarricolea <jjorgesarricolea18@email.com>
2024-05-16 12:13:16 -0600

    feat (middleware): add additional CORS middleware to ensure proper header inclusion
        - Added additional CORS middleware to set necessary CORS headers in all server responses:
        - Access-Control-Allow-Origin with value 'clientUrl' to allow requests from your front-end application hosted on https://sweetfit.netlify.app.
        - Access-Control-Allow-Methods with allowed methods (GET, POST, PUT, DELETE).
        - Access-Control-Allow-Headers with allowed headers (Content-Type, Authorization).

--- server/src/index.js
+++ server/src/index.js
@@ -24,6 +24,13 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", clientUrl);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 // Endpoints
 app.use("/api/auth", authRoutes);
 app.use("/api", usersRoutes);
