e91c3eef138cd4ebf6c77c85d65965af0c37b292
Jorge Sarricolea <jjorgesarricolea18@email.com>
2024-05-16 12:22:39 -0600

    feat (middleware): Change CORS methods
        Changed CORS configuration an methods allowed.

--- server/src/index.js
+++ server/src/index.js
@@ -21,15 +21,10 @@ const usersCouponsRoutes = require("./routes/usersCoupons.js");
 app.use(
   cors({
     origin: clientUrl,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", clientUrl);
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
 
 // Endpoints
 app.use("/api/auth", authRoutes);
